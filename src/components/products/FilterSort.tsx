e67bea26036700e58a6546245213f97bb4c44ad5
JewelHFahim <jewelhfahim@gmail.com>
2025-02-13 01:41:42 +0600

    Slider and HomePage Product

--- src/components/products/FilterSort.tsx
+++ src/components/products/FilterSort.tsx
@@ -1,6 +1,7 @@
 const FilterSort = () => {
   return (
     <div className="flex items-center justify-between py-4">
+      
       <div className="flex items-center gap-2">
         <p className="text-xs uppercase text-gray-400">Sort By:</p>
         <select className="text-sm text-primary">
--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -2,13 +2,24 @@ import { Link } from "react-router";
 
 const Footer = () => {
   return (
-    <div className="mt-16 bg-primary h-[40px]">
-      <div className="sm:w-full md:w-[85vw] lg:w-[80vw] xl:w-[70vw] mx-auto w-full h-full flex justify-between items-center text-white text-sm">
+    <div className="mt-16 bg-primary h-[70px]">
+      <div className="sm:w-full md:w-[85vw] lg:w-[80vw] xl:w-[70vw] mx-auto w-full h-full flex flex-col-reverse md:flex-row justify-center md:justify-between items-center gap-2 text-white text-sm">
         <p>pencilwoodbd @ all right reserve</p>
 
         <div className="flex items-center gap-5">
-          <Link to="">Privacy & Policy</Link>
-          <Link to="">Terms & Condiotion</Link>
+          <Link
+            to=""
+            className="hover:text-teal-100 underline underline-offset-2"
+          >
+            Privacy & Policy
+          </Link>
+          |
+          <Link
+            to=""
+            className="hover:text-teal-100 underline underline-offset-2"
+          >
+            Terms & Condiotion
+          </Link>
         </div>
       </div>
     </div>
