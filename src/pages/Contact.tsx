2eed99b379480ccba3f47b3dc2dfdadd1bf29208
JewelHFahim <jewelhfahim@gmail.com>
2025-02-22 02:21:37 +0600

    about, contact

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,5 +1,7 @@
 import { FaWhatsapp } from "react-icons/fa";
-import { MdOutlineAlternateEmail, MdPayments } from "react-icons/md";
+import { MdOutlineAlternateEmail } from "react-icons/md";
+import { GrLocation } from "react-icons/gr";
+
 import { RiCustomerService2Fill } from "react-icons/ri";
 import SocialIcons from "../utils/SocialIcons";
 
@@ -11,7 +13,7 @@ const Contact = () => {
       </h1>
 
       <div className="my-8 grid gap-5 grid-cols-1 md:grid-cols-3 text-gray-500">
-        <div className="bg-white px-8 py-12 shadow-sm">
+        <div className="bg-white px-8 py-12 shadow-sm hover:shadow-md">
           <div className="flex gap-3">
             <RiCustomerService2Fill className="text-[40px] text-primary" />
             <h2 className="text-xl font-bold text-gray-600 hover:text-primary">
@@ -21,7 +23,7 @@ const Contact = () => {
           <p className="mt-4 font-medium  text-center">+88 01911 209322</p>
         </div>
 
-        <div className="bg-white px-8 py-12 shadow-sm">
+        <div className="bg-white px-8 py-12 shadow-sm hover:shadow-md">
           <div className="flex gap-3">
             <FaWhatsapp className="text-[40px] text-primary" />
             <h2 className="text-xl font-bold text-gray-600 hover:text-primary">
@@ -31,25 +33,27 @@ const Contact = () => {
           <p className="mt-4 font-medium text-center">+88 01911 209322</p>
         </div>
 
-        <div className="bg-white px-8 py-12 shadow-sm">
+        <div className="bg-white px-8 py-12 shadow-sm hover:shadow-md">
           <div className="flex gap-3">
             <MdOutlineAlternateEmail className="text-[40px] text-primary" />
             <h2 className="text-xl font-bold text-gray-600 hover:text-primary">
               Email
             </h2>
           </div>
-          <p className="mt-4 font-medium">support@firecutter.com</p>
+          <p className="mt-4 font-medium text-center">support@firecutter.com</p>
         </div>
       </div>
 
-      <div className="bg-white px-16 py-12 shadow-sm text-gray-500 w-max mx-auto">
+      <div className="bg-white px-8 md:px-16 py-12 shadow-sm text-gray-500 hover:shadow-md">
         <div className="flex gap-3">
-          <MdPayments className="text-[40px] text-primary" />
+          <GrLocation className="text-[40px] text-primary" />
           <h2 className="text-xl font-bold text-gray-600 hover:text-primary">
-            Payment method
+            Location
           </h2>
         </div>
-        <p className="mt-4 font-medium text-center">bKash - 01911209322</p>
+        <p className="mt-4 font-medium md:pl-4">
+          27 Shaptak Square, Level-7, Holding-02, Road-27, Dhanmondi , Dhaka
+        </p>
       </div>
 
       <div className="w-max mx-auto my-5">
